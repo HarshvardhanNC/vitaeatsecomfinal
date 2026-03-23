d199ec5ad6559243d8e325a649394f0f335fec86
ommanglani <ommanglani88@gmail.com>
2026-03-24 00:10:30 +0530

    forced sux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import axios from 'axios'
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL || '';
+if (window.location.hostname !== 'localhost') {
+  axios.defaults.baseURL = 'https://vitaeatsbackend.onrender.com';
+} else {
+  axios.defaults.baseURL = '';
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
